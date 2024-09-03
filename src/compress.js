3f62ec6278c9bac1621a451b529e8c7701ee43e8
Rushs321 <fyggh566@tutamail.com>
2024-09-03 16:02:31 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -3,14 +3,13 @@ import sharp from 'sharp';
 import { redirect } from './redirect.js';
 
 export async function compressImg(request, reply, input) {
-    const { webp, grayscale, quality, originSize } = request.params;
-    const imgFormat = webp ? 'webp' : 'jpeg';
+    const imgFormat = request.params.webp ? 'webp' : 'jpeg';
 
     try {
         const sharpInstance = sharp()
-            .grayscale(grayscale)
+            .grayscale(request.params.grayscale)
             .toFormat(imgFormat, {
-                quality,
+                quality: request.params.quality,
                 progressive: true,
                 optimizeScans: true,
                 chromaSubsampling: '4:4:4'
@@ -24,8 +23,8 @@ export async function compressImg(request, reply, input) {
         reply
             .header('content-type', 'image/' + format)
             .header('content-length', info.size)
-            .header('x-original-size', originSize)
-            .header('x-bytes-saved', originSize - info.size)
+            .header('x-original-size', request.params.originSize)
+            .header('x-bytes-saved', request.params.originSize - info.size)
             .code(200)
             .send(data);
     } catch (error) {
