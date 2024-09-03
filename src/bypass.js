3b986439f5cd007b4ca7a0f14139bf51068729d2
Rushs321 <fyggh566@tutamail.com>
2024-09-03 16:17:11 +0530

    Update bypass.js

--- src/bypass.js
+++ src/bypass.js
@@ -1,7 +1,10 @@
 "use strict";
 
 export function bypass(request, reply, stream) {
+    const contentLength = request.params.originSize || 0; // Fallback to 0 if not provided
     reply.header('x-proxy-bypass', 1);
-    reply.header('content-length', contentLength);
+    if (contentLength > 0) {
+        reply.header('content-length', contentLength);
+    }
     return reply.code(200).send(stream);
 }
