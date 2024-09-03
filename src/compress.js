b45747b56f2b5bf0d9d18e33d9eee8a43791a527
Rushs321 <fyggh566@tutamail.com>
2024-09-03 16:40:00 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -3,17 +3,15 @@ import sharp from 'sharp';
 import { redirect } from './redirect.js';
 
 export async function compressImg(request, reply, input) {
-    const { webp, grayscale, quality, originSize } = request.params;
-    const imgFormat = webp ? 'webp' : 'jpeg';
+    const format = request.params.webp ? 'webp' : 'jpeg'
 
     try {
         const sharpInstance = sharp()
-            .grayscale(grayscale)
-            .toFormat(imgFormat, {
-                quality,
+            .grayscale(request.params.grayscale)
+            .toFormat(format, {
+                quality: request.params.quality,
                 progressive: true,
-                optimizeScans: true,
-                chromaSubsampling: '4:4:4'
+                optimizeScans: true
             });
 
         // Pipe the stream from input.body into the sharp instance
@@ -22,10 +20,10 @@ export async function compressImg(request, reply, input) {
         const { data, info } = await sharpInstance.toBuffer({ resolveWithObject: true });
 
         reply
-            .header('content-type', `image/${imgFormat}`)
+            .header('content-type', 'image/' + format)
             .header('content-length', info.size)
-            .header('x-original-size', originSize)
-            .header('x-bytes-saved', originSize - info.size)
+            .header('x-original-size', request.params.originSize)
+            .header('x-bytes-saved', request.params.originSize - info.size)
             .code(200)
             .send(data);
     } catch (error) {
